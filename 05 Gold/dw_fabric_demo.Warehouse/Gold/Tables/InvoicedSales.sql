2ac16555666594ce496672e210c5222468da69ae
Howard Taylor <howardtaylor@microsoft.com>
2026-02-16 22:10:51 -0500

    Committing 2 items from workspace ecfcf483-666f-4267-9456-d09b370c4a12

--- 05 Gold/dw_fabric_demo.Warehouse/Gold/Tables/InvoicedSales.sql
+++ 05 Gold/dw_fabric_demo.Warehouse/Gold/Tables/InvoicedSales.sql
@@ -1,14 +1,14 @@
 CREATE TABLE [Gold].[InvoicedSales] (
 
-	[InvoiceID]           int           NOT NULL,
-	[InvoiceLineID]       int           NOT NULL,
-	[InvoiceDate]         date          NULL,
-	[CustomerID]          int           NULL,
-	[StockItemID]         int           NULL,
-	[SalespersonPersonID] int           NULL,
-	[Quantity]            int           NULL,
-	[ExtendedPrice]       decimal(18,2) NULL,
-	[GrossProfit]         decimal(18,2) NULL,
-	[TaxAmount]           decimal(18,2) NULL,
-	[LastUpdated]         datetime2(6)  NULL
+	[InvoiceID] int NOT NULL, 
+	[InvoiceLineID] int NOT NULL, 
+	[InvoiceDate] date NULL, 
+	[CustomerID] int NULL, 
+	[StockItemID] int NULL, 
+	[SalespersonPersonID] int NULL, 
+	[Quantity] int NULL, 
+	[ExtendedPrice] decimal(18,2) NULL, 
+	[GrossProfit] decimal(18,2) NULL, 
+	[TaxAmount] decimal(18,2) NULL, 
+	[LastUpdated] datetime2(6) NULL
 );
